538fb91cd3f7aef8f63241b6a3d9fd999c40cb13
Said Sow <saidevproject@gmail.com>
2025-04-09 10:08:57 +0200

    New Test

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+function checkParamIsNumber() {
+  for (p of arguments) {
+    if (typeof p !== 'number') {
+      throw new Error('Param should be a number')
+    }
+  }
+}
+
 function sum(a, b) {
+  checkParamIsNumber(a, b)
   return a + b
 }
 
 function sub(a, b) {
+  checkParamIsNumber(a, b)
   return a - b
 }
 
--- index.test.js
+++ index.test.js
@@ -4,6 +4,14 @@ test('add 1 + 2 = 3', () => {
   expect(sum(1, 2)).toBe(3)
 })
 
+test('add with not number values', () => {
+  expect(() => sum('hello', { a: 'b' })).toThrow('Param should be a number')
+})
+
 test('sub 5 - 2 = 3', () => {
   expect(sub(5, 2)).toBe(3)
+})
+
+test('add with not number values', () => {
+  expect(() => sum({ a: 'b' }, new Date())).toThrow('Param should be a number')
 })
